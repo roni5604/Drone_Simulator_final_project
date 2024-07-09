f3254d71b484922ab2548248ca0d12a22f858d1e
elorisraeli <elorisraeli@gmail.com>
2024-07-09 15:17:51 +0300

    images folder

--- Simulator_3D/world_params.py
+++ Simulator_3D/world_params.py
@@ -1,8 +1,8 @@
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 
-DRONE_PICTURE = 'drone_pic.png'
-WARNING_PICTURE = 'warning.png'
+DRONE_PICTURE = 'Images/drone_pic.png'
+WARNING_PICTURE = 'Images/warning.png'
 
 APARTMENT1_FLOOR = [[1] * 20 for _ in range(20)]
 
